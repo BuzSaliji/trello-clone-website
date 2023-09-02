b28b6050f4a0d74ea255ca518f68b8044032cd8d
BuzSaliji <buz.saliji@gmail.com>
2023-09-02 11:20:42 +1000

    feat: wipe the data row

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(trelloData);
+
+// Data rendering file
+// Read trelloData, find the data and display row and
+// generate html elements based on the trelloData
+
+function renderColumns() {
+  let trelloDataRowRootNode = document.getElementById("dataDisplayRow");
+
+  trelloDataRowRootNode.innerHTML = "";
+}
+
+renderColumns();
